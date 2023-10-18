200a2092ca8f6f65a11981086b34f9379e3d5d15
Arth Kaushik <122805250+ark4u4help@users.noreply.github.com>
2023-10-18 23:39:41 +0530

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,18 @@
+package appcode;
+
+public class SimpleCalculator {
+   public int  add(int n , int m) {
+		return n+m;
+	}
+   public int  sub(int n , int m) {
+		return n-m;
+	}
+   public int  mul(int n , int m) {
+		return n*m;
+	}
+   public int  div(int n , int m) {
+		return n/m;
+	}
+   
+
+}
